40e3147e3724478fc87727b199337e01dd13c31f
Matthew Harwood <matthhar12@gmail.com>
2015-02-28 16:16:11 -0800

    init commit

---
+++ public/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicApp
+ * @description
+ * # publicApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/room/:roomId', {
+        templateUrl: 'views/room.html',
+        controller: 'RoomCtrl'
+      })
+      .when('/room', {
+        templateUrl: 'views/room.html',
+        controller: 'RoomCtrl'
+      })
+      .otherwise({
+        redirectTo: '/room'
+      });
+  });
+angular.module('publicApp')
+  .constant('config', {
+      // Change it for your app URL
+      SIGNALIG_SERVER_URL: YOUR_APP_URL
+  });
